72e33474ecf00a7681d70f5ac4543048eb3c0676
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-21 21:16:02 +0300

    T7 fix 0

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,24 +7,27 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_route():
     return "HBNB is the best"
 
+
 @app.route('/states_list', strict_slashes=False)
 def data_to_html_route():
     """open the data to view it in html"""
-    
+
     states = storage.all(State).values()
-    
+
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html", states=sorted_states)
-    
+
 
 @app.teardown_appcontext
 def close_storage(exception):
     """Closes the storage session after each request"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
