c5cb1fd58619f64b4e195901b3213e8fdf716e9c
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-21 14:31:37 +0300

    2024-05-21 02:31:37 PM

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -27,6 +27,5 @@ def c_is_fun(text):
 
 
 if __name__ == "__main__":
-    # to run the flask app with all hosts and in specific port
     app.run(host='0.0.0.0', port=5000)
-    
+    # to run the flask app with all hosts and in specific port
