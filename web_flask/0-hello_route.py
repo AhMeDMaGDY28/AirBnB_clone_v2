21f9c1be01ef867a431b0f9febf9a6cfb4547d30
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-21 12:04:57 +0300

    0 fix 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def sayhello():
-	"""a func to say hello in html"""
-	return "Hello HBNB!"
+    """a func to say hello in html"""
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
