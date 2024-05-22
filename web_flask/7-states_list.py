89460a740205b64f9ab65264aadfcf4a1b300d97
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-22 17:03:02 +0300

    fixing 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,19 +8,22 @@ from models.state import State
 app = Flask(__name__)
 
 
-# the home route
 @app.route('/')
 def home_route():
-    return "HBNB is good"
+    return "HBNB is the best"
 
 
-# the states route
 @app.route('/states_list', strict_slashes=False)
-def states_to_html_route():
+def data_to_html_route():
     """open the data to view it in html"""
+    
+
     states = storage.all(State).values()
+    
+
     sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template("8-cities_by_states.html", states=sorted_states)
+    return render_template("7-states_list.html", states=sorted_states)
+    
 
 
 @app.teardown_appcontext
